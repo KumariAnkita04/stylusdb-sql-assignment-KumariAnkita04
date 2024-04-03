760519bed65bd64912f004077cc42ec097d5cba3
Kumari Ankita <kumariankita0401@gmail.com>
2024-04-03 17:30:56 +0530

    feat: step-3

--- src/queryParser.js
+++ src/queryParser.js
@@ -1,30 +1,13 @@
-// function parseQuery(query) {
-//     const selectRegex = /SELECT (.+) FROM (.+)/i;
-//     const match = query.match(selectRegex);
-
-//     if (match) {
-//         const [, fields, table] = match;
-//         return {
-//             fields: fields.split(',').map(field => field.trim()),
-//             table: table.trim()
-//         };
-//     } else {
-//         throw new Error('Invalid query format');
-//     }
-// }
-
-// module.exports = parseQuery;/
-
 function parseQuery(query) {
-    const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
+    const selectRegex = /SELECT (.+) FROM (.+)/i;
     const match = query.match(selectRegex);
 
     if (match) {
-        const [, fields, table, whereClause] = match;
+        const [, fields, table] = match;
         return {
             fields: fields.split(',').map(field => field.trim()),
             table: table.trim(),
-            whereClause: whereClause ? whereClause.trim() : null
+            
         };
     } else {
         throw new Error('Invalid query format');
@@ -32,3 +15,4 @@ function parseQuery(query) {
 }
 
 module.exports = parseQuery;
+
