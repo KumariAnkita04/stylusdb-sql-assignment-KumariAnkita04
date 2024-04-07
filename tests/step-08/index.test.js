9e84f74826093dedbdffbc52a3b23a2ace36eb9b
Kumari Ankita <kumariankita0401@gmail.com>
2024-04-07 12:54:20 +0530

    feat: step-8

--- tests/step-08/index.test.js
+++ tests/step-08/index.test.js
@@ -1,6 +1,10 @@
 const readCSV = require('../../src/csvReader');
 const parseQuery = require('../../src/queryParser');
+
 const executeSELECTQuery = require('../../src/index');
+
+
+
 
 test('Read CSV File', async () => {
     const data = await readCSV('./student.csv');
@@ -26,6 +30,10 @@ test('Execute SQL Query', async () => {
     const query = 'SELECT id, name FROM student';
     const result = await executeSELECTQuery(query);
     expect(result.length).toBeGreaterThan(0);
+    // expect(result.length).toBeGreaterThan(0);
+   
+
+
     expect(result[0]).toHaveProperty('id');
     expect(result[0]).toHaveProperty('name');
     expect(result[0]).not.toHaveProperty('age');
