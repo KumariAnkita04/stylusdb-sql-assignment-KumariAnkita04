4acb1a6371fc3819c991df0cf35081acf8d2f30b
Kumari Ankita <kumariankita0401@gmail.com>
2024-04-11 10:33:57 +0530

    feat: step-9

--- src/queryParser.js
+++ src/queryParser.js
@@ -1,95 +1,174 @@
-
-function parseQuery(query) {
-    // First, let's trim the query to remove any leading/trailing whitespaces
-    query = query.trim();
-
-    // Initiaalize variables for different parts of the query
-    let selectPart, fromPart;
-
-    // Split the query at the WHERE clause if it exists
-    const whereSplit = query.split(/\sWHERE\s/i);
-    query = whereSplit[0]; // Everything before WHERE clause
-
-    // WHERE clause is the second part after splitting, if it exists
-    const whereClause = whereSplit.length > 1 ? whereSplit[1].trim() : null;
-
-    // Split the remaining query at the JOIN clause if it exists
-    const joinSplit = query.split(/\sINNER JOIN\s/i);
-    selectPart = joinSplit[0].trim(); // Everything before JOIN clause
-
-    // JOIN clause is the second part after splitting, if it exists
-    const joinPart = joinSplit.length > 1 ? joinSplit[1].trim() : null;
-
-    // Parse the SELECT part
-    const selectRegex = /^SELECT\s(.+?)\sFROM\s(.+)/i;
-    const selectMatch = selectPart.match(selectRegex);
-    if (!selectMatch) {
-        throw new Error('Invalid SELECT format');
+function parseWhereClause(whereString) {
+    const conditionRegex = /(.*?)(=|!=|>|<|>=|<=)(.*)/;
+    return whereString.split(/ AND | OR /i).map((conditionString) => {
+      if (conditionString.includes(" LIKE ")) {
+        const condition = conditionString.split(/\sLIKE\s/i);
+        const [field, pattern] = condition;
+        return { field: field.trim(), operator: "LIKE", value: pattern.trim() };
+      }
+      const match = conditionString.match(conditionRegex);
+      if (match) {
+        const [, field, operator, value] = match;
+        return { field: field.trim(), operator, value: value.trim() };
+      }
+      throw new Error("Invalid WHERE clause format");
+    });
+  }
+  
+  function parseJoinClause(query) {
+    const joinRegex =
+      /\s(INNER|LEFT|RIGHT) JOIN\s(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
+    const joinMatch = query.match(joinRegex);
+    if (joinMatch) {
+      return {
+        joinType: joinMatch[1].trim(),
+        joinTable: joinMatch[2].trim(),
+        joinCondition: {
+          left: joinMatch[3].trim(),
+          right: joinMatch[4].trim(),
+        },
+      };
     }
-
-    const [, fields, table] = selectMatch;
-
-    // Parse the JOIN part if it exists
-    let joinTable = null, joinCondition = null;
-
-    if (joinPart) {
-        const joinRegex = /^(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-        const joinMatch = joinPart.match(joinRegex);
-        if (!joinMatch) {
-            throw new Error('Invalid JOIN format');
-        }
-
-        joinTable = joinMatch[1].trim();
-        joinCondition = {
-            left: joinMatch[2].trim(),
-            right: joinMatch[3].trim()
-        };
-    }
-
-    // Parse the WHERE part if it exists
-    let whereClauses = [];
-    if (whereClause) {
-        whereClauses = parseWhereClause(whereClause);
-    }
-
+  
     return {
-        fields: fields.split(',').map(field => field.trim()),
+      joinType: null,
+      joinTable: null,
+      joinCondition: null,
+    };
+  }
+  
+  function parseInsertQuery(query) {
+    const regex = /INSERT INTO (\w+)\s*(?:\(([^)]+)\))?\s*VALUES\s*\(([^)]+)\)/i;
+    const match = query.match(regex);
+  
+    if (match) {
+      const table = match[1];
+      const columns = match[2]?.split(/\s*,\s*/).map((col) => `'${col}'`) || [];
+      const values = match[3].split(/\s*,\s*/).map((val) => `'${val}'`);
+      return {
+        type: "INSERT",
+        table,
+        columns,
+        values,
+      };
+    }
+  }
+  
+  function parseDeleteQuery(query) {
+    const pattern = /^DELETE\s+FROM\s+(.+?)\s+WHERE\s+(.+?)$/i;
+    const match = query.trim().match(pattern);
+  
+    if (!match) {
+      throw new Error("Invalid DELETE query format");
+    }
+  
+    const table = match[1];
+    const whereClause = match[2];
+  
+    const whereClauses = parseWhereClause(whereClause);
+    return {
+      type: "DELETE",
+      table,
+      whereClauses,
+    };
+  }
+  
+  function getGroupByFields(groupBySplit) {
+    if (groupBySplit.length > 1) {
+      return groupBySplit[1].split(",").map((field) => field.trim());
+    } else return null;
+  }
+  
+  function parseSelectQuery(query) {
+    try {
+      query = query.trim();
+  
+      let selectPart, fromPart;
+      let hasAggregateWithoutGroupBy = false;
+      let isDistinct = false;
+      if (query.toUpperCase().includes("SELECT DISTINCT")) {
+        isDistinct = true;
+        query = query.replace("SELECT DISTINCT", "SELECT");
+      }
+      const limitSplit = query.split(/\LIMIT\s/i);
+      query = limitSplit[0];
+      let limit = null;
+      if (limitSplit[1]) {
+        limit = parseInt(limitSplit[1]);
+      }
+  
+      const orderByMatch = query.split(/\ORDER BY\s/i);
+      query = orderByMatch[0];
+      let orderByFields = null;
+  
+      if (orderByMatch[1]) {
+        orderByFields = orderByMatch[1].split(",").map((field) => {
+          const [fieldName, order] = field.trim().split(/\s+/);
+          return { fieldName, order: order ? order.toUpperCase() : "ASC" };
+        });
+      }
+  
+      const groupBySplit = query.split(/\GROUP BY\s/i);
+      query = groupBySplit[0];
+      const groupByFields = getGroupByFields(groupBySplit);
+  
+      const whereSplit = query.split(/\sWHERE\s/i);
+      query = whereSplit[0];
+  
+      const whereClause = whereSplit.length > 1 ? whereSplit[1].trim() : null;
+  
+      const joinSplit = query.split(/\s(INNER|LEFT|RIGHT) JOIN\s/i);
+      selectPart = joinSplit[0].trim();
+  
+      const joinPart = joinSplit.length > 1 ? joinSplit[1].trim() : null;
+      const joinClause = parseJoinClause(query);
+      const joinType = joinClause.joinType;
+      const joinTable = joinClause.joinTable;
+      const joinCondition = joinClause.joinCondition;
+      const selectRegex = /^SELECT\s(.+?)\sFROM\s(.+)/i;
+      const selectMatch = selectPart.match(selectRegex);
+      if (!selectMatch) {
+        throw new Error("Invalid SELECT format");
+      }
+  
+      const [, fields, table] = selectMatch;
+  
+      let whereClauses = [];
+  
+      if (whereClause) {
+        whereClauses = parseWhereClause(whereClause);
+      }
+  
+      const temp = fields.split(",").map((field) => field.trim());
+  
+      temp.map((field) => {
+        const match = field.match(/^(AVG|SUM|COUNT|MIN|MAX)\((.+)\)/i);
+        if (match && !groupByFields) {
+          hasAggregateWithoutGroupBy = true;
+        }
+      });
+  
+      return {
+        fields: fields.split(",").map((field) => field.trim()),
         table: table.trim(),
         whereClauses,
         joinTable,
-        joinCondition
-    };
-}
-function parseJoinClause(query) {
-  const joinRegex = /\s(INNER|LEFT|RIGHT) JOIN\s(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-  const joinMatch = query.match(joinRegex);
-
-  if (joinMatch) {
-      return {
-          joinType: joinMatch[1].trim(),
-          joinTable: joinMatch[2].trim(),
-          joinCondition: {
-              left: joinMatch[3].trim(),
-              right: joinMatch[4].trim()
-          }
+        joinCondition,
+        joinType,
+        groupByFields,
+        hasAggregateWithoutGroupBy,
+        orderByFields,
+        limit,
+        isDistinct,
       };
+    } catch (error) {
+      throw new Error(`Query parsing error: ${error.message}`);
+    }
   }
-
-  return {
-      joinType: null,
-      joinTable: null,
-      joinCondition: null
+  
+  module.exports = {
+    parseSelectQuery,
+    parseJoinClause,
+    parseInsertQuery,
+    parseDeleteQuery,
   };
-}
-
-
-function parseWhereClause(whereString) {
-    const conditions = whereString.split(/ AND | OR /i);
-    return conditions.map(condition => {
-        const [field, operator, value] = condition.split(/\s+/);
-        return { field, operator, value };
-    });
-}
-
-module.exports =  { parseQuery, parseJoinClause };
-
--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,20 +1,36 @@
-const readCSV = require('../../src/csvReader');
-const parseQuery = require('../../src/queryParser');
+const { readCSV } = require("../../src/csvReader");
+const { executeSELECTQuery } = require("../../src/index");
+const { parseJoinClause, parseSelectQuery } = require("../../src/queryParser");
 
-test('Read CSV File', async () => {
-    const data = await readCSV('./student.csv');
-    expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+test("Read CSV File", async () => {
+  const data = await readCSV("./student.csv");
+  expect(data.length).toBeGreaterThan(0);
+  expect(data.length).toBe(4);
+  expect(data[0].name).toBe("John");
+  expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
 });
 
+test("Parse SQL Query", () => {
+  const query = "SELECT id, name FROM student";
+  const parsed = parseSelectQuery(query);
+  expect(parsed).toEqual({
+    fields: ["id", "name"],
+    table: "student",
+    whereClauses: [],
+    joinCondition: null,
+    orderByFields: null,
+    joinTable: null,
+    joinType: null,
+    groupByFields: null,
+    hasAggregateWithoutGroupBy: false,
+    limit: null,
+    isDistinct: false,
+  });
+});
 
-
-test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM student';
-    const parsed = parseQuery(query);
-    expect(parsed.fields).toEqual(['id', 'name']);
-    expect(parsed.table).toBe('student');
-    expect(parsed.whereClauses).toEqual([]);
+test("Parse Invalid SQL Query", () => {
+  const Invalidquery = "TARGET id, name OF student";
+  expect(() => {
+    parseSelectQuery(Invalidquery);
+  }).toThrow("Invalid SELECT format");
 });
